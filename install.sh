d6fa0f09fcd3504538cdf61415ffded8bd20b697
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2015-01-25 17:35:56 +1100

    better docs for install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+##############################################
+# Install a course found in the courses folder
+# $1: name of course
 function install_course () {
     # Check that course exists
     if [ ! -e "./courses/$1.sh" ]; then
@@ -12,13 +15,18 @@ function install_course () {
     echo "configured"
 }
 
+##############################################
+# Main script
+# Installs the "base" course by default
+# All other args are assumed to be courses to install
+
 # Check if executing as root
 if [ "$(id -u )" != "0" ]; then
-    echo "Must execut as root"
+    echo "must execute as root"
     exit 1
 fi
 
-
+# Install courses
 for course in base $*; do
     install_course $course
 done
